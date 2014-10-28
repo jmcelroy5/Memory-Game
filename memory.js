8564d7a89f0a5140d0aeedea048e5fb0f984320d
Jessica McElroy <mcelroyjessica@gmail.com>
2014-10-27 22:27:54 -0700

    Rewrote the function that creates tile divs to map over the letter array. Pass whole tile object to MemoryFlip instead of getting it by id

--- memory.js
+++ memory.js
@@ -1,17 +1,20 @@
 
 var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
 var memory_values = [];
-var memory_indices= [];
+var flipped_tiles = [];
 var tiles_matched = 0;
 
 function newBoard(){
 	shuffle(memory_array);
-	for (var i=0; i < memory_array.length ; i++){
-		var tile = document.createElement("div");
-		tile.setAttribute("id", i);
-		tile.setAttribute('onclick','MemoryFlip(this,"' + memory_array[i] + '")');
+																// The following creates a new array of tile objects that mirrors the memory array
+	var tileDivs = memory_array.map(function(value,index){		// Map over each item in memory array (param: current memory value and current index)
+		var tile = document.createElement("div");				// Create a new tile div object
+		tile.addEventListener('click', function() {				// Then add a click event listener to it which will...
+			MemoryFlip(tileDivs[index], value);					// Call MemoryFlip, passing tile object and corresponding letter value
+		});
 		document.getElementById("container").appendChild(tile);
-	}
+		return tile;	
+	});
 }
 
 function shuffle(o){
@@ -25,26 +28,26 @@ function MemoryFlip(tile, value){
 		tile.style.background = "#ffffff";
 		tile.innerHTML = value;
 		memory_values.push(value);
-		memory_indices.push(tile.id);
+		flipped_tiles.push(tile)
 	}
 
 	if (memory_values.length == 2){
 		if (memory_values[0] == memory_values[1]){
 			tiles_matched += 2;
 			memory_values = [];
-			memory_indices = [];
+			flipped_tiles = [];
 		}
 		else if (memory_values[0] != memory_values[1]) {
 
 			function flipBack() {
-				var tile1 = document.getElementById(memory_indices[0]);
-				var tile2 = document.getElementById(memory_indices[1]);
+				var tile1 = flipped_tiles[0]
+				var tile2 = flipped_tiles[1]
 				tile1.style.background = "url(cat.jpg)";
 				tile2.style.background = "url(cat.jpg)";
 				tile1.innerHTML = "";
 				tile2.innerHTML = "";
 				memory_values = [];
-				memory_indices = [];
+				flipped_tiles = [];
 			}
 			setTimeout(flipBack, 700);
 		}
